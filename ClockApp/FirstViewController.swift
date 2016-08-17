fd308ad40052e27aea740ef4ccdfd759c02066c5
Paul Maxeiner <paulmaxeiner@pauls-macbook-air.local>
2016-08-17 12:46:51 -0400

    Finished Current Time Screen and Functionality

--- ClockApp/FirstViewController.swift
+++ ClockApp/FirstViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(FirstViewController.updateTime), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,9 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateTime() {
+        timeLabel.text = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .ShortStyle, timeStyle: .FullStyle)
+    }
 
 }
 
